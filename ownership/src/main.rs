fc39aaf9a11d43ecfd873ba412048cb45f06cf6a
Kaleb Dykema <dykemakaleb@gmail.com>
2022-05-26 00:09:50 -0500

    Tested out some ownership concepts

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    // // This creates a mutable string. Because this is mutable, we don't know the size! This is actually stored on the heap instead of the stack.
+    // let mut s = String::from("I'm a mutable string!");
+    // println!("{}", s);
+    // s.push_str(" See?");
+    // println!("{}", s);
+
+    // {
+    //     let s = String::from("I'm limited to this scope and will disappear");
+
+    //     // The above variable gets "droped" from memory at the end of this block. Imagine the follow line is being run
+    //     // drop s;
+    // }
+
+    // let x = 5;
+    // let y = 5;
 }
